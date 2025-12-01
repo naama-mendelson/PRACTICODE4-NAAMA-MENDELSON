6ec8b53bd6a546d123a793b38ef417b4537bfdce
Naama Mendelson <n0556731758@gmail.com>
2025-12-01 02:21:56 +0200

    FIX cors for render client

--- TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
+++ TodoApi/obj/Debug/net9.0/TodoApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+183951f1c4532fa5a26675989fcd7e66eb6ec835")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09af795e10891da26e2525150cf86f63299cd7fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
